da93f7e40146292f8ec98c5abae2c622bb3dc62c
halltony <a.j.hall@blueyonder.co.uk>
2016-08-14 21:28:26 +0100

    Complete initial

---
+++ cookbooks/chefnexus/metadata.rb
@@ -0,0 +1,7 @@
+name             'chefnexus'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures chefnexus'
+long_description 'Installs/Configures chefnexus'
+version          '0.1.0'
